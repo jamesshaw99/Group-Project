6f54d670225495ce3abeb3c0e5085c3b571dd3c0
James <james.shaw1234@hotmail.co.uk>
2019-03-27 15:49:03 +0000

    Adding movement and interactability

---
+++ VR Escape Room/Assets/door.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public GameObject theDoor;
+    Animator otherAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        otherAnimator = theDoor.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x < theDoor.transform.position.x + 5 && this.transform.position.x > theDoor.transform.position.x - 5)
+        {
+            otherAnimator.SetBool("character_nearby", true);
+        }
+        else
+        {
+            otherAnimator.SetBool("character_nearby", false);
+        }
+    }
+}
