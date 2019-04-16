b5c438b017a42934629475fdddc90a8f61902095
James <james.shaw1234@hotmail.co.uk>
2019-04-16 12:05:11 +0100

    project update 16/04/19
    Adding glass walls to the room to section off puzzle areas, and adding animations to the door with triggers when the player is within a certain distance
    
    Co-Authored-By: johndvos <johndvos@users.noreply.github.com>

--- VR Escape Room/Assets/door.cs
+++ VR Escape Room/Assets/door.cs
@@ -4,24 +4,72 @@ using UnityEngine;
 
 public class door : MonoBehaviour
 {
-    public GameObject theDoor;
-    Animator otherAnimator;
+    public GameObject theMidroomDoor;
+    public GameObject doorRoom1;
+    public GameObject doorRoom2;
+    public GameObject doorRoom3;
+    public GameObject doorRoom4;
+    Animator midAnimator;
+    Animator room1Animator;
+    Animator room2Animator;
+    Animator room3Animator;
+    Animator room4Animator;
     // Start is called before the first frame update
     void Start()
     {
-        otherAnimator = theDoor.GetComponent<Animator>();
+        midAnimator = theMidroomDoor.GetComponent<Animator>();
+        room1Animator = doorRoom1.GetComponent<Animator>();
+        room2Animator = doorRoom2.GetComponent<Animator>();
+        room3Animator = doorRoom3.GetComponent<Animator>();
+        room4Animator = doorRoom4.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (this.transform.position.x < theDoor.transform.position.x + 5 && this.transform.position.x > theDoor.transform.position.x - 5)
+        if (this.transform.position.x < theMidroomDoor.transform.position.x + 5 && this.transform.position.x > theMidroomDoor.transform.position.x - 5)
         {
-            otherAnimator.SetBool("character_nearby", true);
+            midAnimator.SetBool("character_nearby", true);
         }
         else
         {
-            otherAnimator.SetBool("character_nearby", false);
+            midAnimator.SetBool("character_nearby", false);
+        }
+
+        if ((this.transform.position.z < doorRoom1.transform.position.z + 2 && this.transform.position.z > doorRoom1.transform.position.z - 2) && (-19 < this.transform.position.x && this.transform.position.x < -16))
+        {
+            room1Animator.SetBool("character_nearby", true);
+        }
+        else
+        {
+            room1Animator.SetBool("character_nearby", false);
+        }
+
+        if ((this.transform.position.z < doorRoom2.transform.position.z + 2 && this.transform.position.z > doorRoom2.transform.position.z - 2) && (-19 < this.transform.position.x && this.transform.position.x < -16))
+        {
+            room2Animator.SetBool("character_nearby", true);
+        }
+        else
+        {
+            room2Animator.SetBool("character_nearby", false);
+        }
+
+        if ((this.transform.position.z < doorRoom3.transform.position.z + 2 && this.transform.position.z > doorRoom3.transform.position.z - 2) && (-1 < this.transform.position.x && this.transform.position.x < 1))
+        {
+            room3Animator.SetBool("character_nearby", true);
+        }
+        else
+        {
+            room3Animator.SetBool("character_nearby", false);
+        }
+
+        if ((this.transform.position.z < doorRoom4.transform.position.z + 2 && this.transform.position.z > doorRoom4.transform.position.z - 2) && (-1 < this.transform.position.x && this.transform.position.x < 1))
+        {
+            room4Animator.SetBool("character_nearby", true);
+        }
+        else
+        {
+            room4Animator.SetBool("character_nearby", false);
         }
     }
 }
