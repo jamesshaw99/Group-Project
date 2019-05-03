74a9ccac1401de381f18a46705980e7920473c95
jackwhitchurch <46376259+jackwhitchurch@users.noreply.github.com>
2019-05-03 19:55:55 +0100

    test
    test

---
+++ VR Escape Room/Assets/Scripts/PickUp.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    //script needs to be added to every object we want to be able to pick up
+    public Transform theDest;
+
+    void OnMouseDown()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false; //turns gravity off so object doesnt just fall when pickedup
+        this.transform.position = theDest.position;
+        this.transform.parent = GameObject.Find("PickUpLocation").transform;
+    }
+
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true; //turns gravity back on 
+        GetComponent<BoxCollider>().enabled = true;
+    }
+}
---
+++ VR Escape Room/Assets/Scripts/ShowCursor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    void OnGUI()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+}
