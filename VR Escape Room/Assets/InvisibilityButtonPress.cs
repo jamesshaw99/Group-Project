51379817f9310f7b7316bc061cab40b706b5e1d9
James <james.shaw1234@hotmail.co.uk>
2019-05-23 16:11:38 +0100

    23/05/2019
    final testing and debugging

--- VR Escape Room/Assets/InvisibilityButtonPress.cs
+++ VR Escape Room/Assets/InvisibilityButtonPress.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class InvisibilityButtonPress : MonoBehaviour
 {
     public GameObject obj;
     public GameObject obj2;
+    public GameObject button;
+    public GameObject leftHand;
+    public GameObject rightHand;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +20,14 @@ public class InvisibilityButtonPress : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.y > 0.7)
-        {
+        Debug.Log("left hand position" + leftHand.transform.position);
+        Debug.Log("button position" + button.transform.position);
+        if (leftHand.transform.position.x < button.transform.position.x + 1 && leftHand.transform.position.x > button.transform.position.x - 1  && leftHand.transform.position.y < button.transform.position.y + 1 && leftHand.transform.position.y > button.transform.position.y - 1 && leftHand.transform.position.z < button.transform.position.z + 1 && leftHand.transform.position.z > button.transform.position.z - 1)
+        {        
             obj.SetActive(true);
             obj2.SetActive(true);
         }
+
+
     }
 }
